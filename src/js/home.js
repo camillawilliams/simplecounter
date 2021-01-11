afe4f8e6e98919487614c281d8545bf2b1dcf35e
camillawilliams <camillawilliamsdu@gmail.com>
2021-01-11 19:46:10 +0000

    font awesome issue

---
+++ src/js/home.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+// export function Home(props) {
+//     console.log(props.counter);
+//     return (
+//         <div className = "text-center mt-5">
+//             <h1>Simple Counter</h1> 
+//         <a href "#" className="Counter" />
+//         </div>
+//     )
+// }
+
+// Home.propTypes = {
+//   counter: PropTypes.number;
+// };
--- src/js/index.js
+++ src/js/index.js
@@ -10,6 +10,42 @@ import "../styles/index.scss";
 
 //import your own components
 import { Home } from "./component/home.js";
+import { library, icon } from "@fortawesome/fontawesome-svg-core";
+import { fontawesome } from "@fontawesome/fontawesome";
+import { faClock } from "@fortawesome/free-regular/faClock";
+fontawesome.config = {
+	autoReplaceSvg: "nest"
+};
+fontawesome.library.add(faClock);
 
+const clock = icon({ prefix: "far", iconName: "clock" });
+
+// fontawesome.library.add(faClock);
+
+// var countZero = 0;
+
+// function counterApp() {
+// countZero++;
+// const string = "000000" + countAero;
+
+// ReactDOM.render(
+// <Home counter={countZero}/>,
+//     document.querySelector("#app")
+// );
+// }
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+// setInterval(counterApp, 1000);
+
+function SimpleCounter(props) {
+	return (
+		<div className="bigCounter">
+			<div className="calendar">
+				<i className="far fa-clock" />
+			</div>
+			<div className="four">0</div>
+			<div className="three">0</div>
+			<div className="two">0</div>
+			<div className="one">0</div>
+		</div>
+	);
+}
